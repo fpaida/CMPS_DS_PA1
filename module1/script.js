da0a549ca1400a5537d54d5b49ac5670a817aa3f
Fabrice Paida <fpaidas@yahoo.fr>
2026-05-14 03:24:17 -0400

    Initial CMPS260 setup

---
+++ module1/script.js
@@ -0,0 +1,13 @@
+let device = {
+    hostname: "router1",
+    ip: "192.168.1.1",
+    os: "Cisco IOS",
+    online: true
+};
+
+console.log(device);
+
+console.log(device.hostname);
+console.log(device.ip);
+console.log(device.os);
+console.log(device.online);
